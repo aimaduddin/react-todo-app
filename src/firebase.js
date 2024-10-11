3eea58d6fb869878bcec216b2e9ab06171e74720
Aimaduddin <aima.aimaduddin@gmail.com>
2024-10-11 20:57:25 +0800

    new build

--- src/firebase.js
+++ src/firebase.js
@@ -17,11 +17,11 @@ const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 enableIndexedDbPersistence(db).catch((err) => {
-    if (err.code == 'failed-precondition') {
+    if (err.code === 'failed-precondition') {
         // Multiple tabs open, persistence can only be enabled
         // in one tab at a a time.
         console.log('Persistence failed');
-    } else if (err.code == 'unimplemented') {
+    } else if (err.code === 'unimplemented') {
         // The current browser does not support all of the
         // features required to enable persistence
         console.log('Persistence is not valid');
